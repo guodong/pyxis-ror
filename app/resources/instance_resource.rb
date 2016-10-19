6a93b1c4b256efcf5c03534176171684a72c0767
guodong <gd@tongjo.com>
2016-10-19 14:23:18 +0800

    add arch param

--- app/resources/instance_resource.rb
+++ app/resources/instance_resource.rb
@@ -13,6 +13,6 @@ class InstanceResource < BaseResource
 
   after_save do
     uri = URI('http://localhost:8081/instances')
-    res = Net::HTTP.post_form(uri, 'host_id' => '29ba3e6a-84b3-451a-a282-c156b35ed6b2', 'sysname' => self.user.sysname, 'cmd' => self.version.cmd, 'width' => self.width, 'height' => self.height, 'token' => self.token)
+    res = Net::HTTP.post_form(uri, 'host_id' => '29ba3e6a-84b3-451a-a282-c156b35ed6b2', 'sysname' => self.user.sysname, 'cmd' => self.version.cmd, 'width' => self.width, 'height' => self.height, 'token' => self.token, 'arch' => self.version.arch)
   end
 end
